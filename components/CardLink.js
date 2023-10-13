5d12ef3009b8f38e56f77b3e0c0ba27e5c7ac980
Thiago Santana <thi.dimond1@gmail.com>
2023-10-13 16:31:21 -0300

    Export cardLink as default

--- components/CardLink.js
+++ components/CardLink.js
@@ -3,7 +3,7 @@ import styles from "../styles/Home.module.css";
 import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
-export function CardLink({ children, title, href }) {
+export default function CardLink({ children, title, href }) {
   return (
     <Link href={href} className={styles.card}>
       <h2 className={inter.className}>
