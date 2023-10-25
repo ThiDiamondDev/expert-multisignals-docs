f623d354f9d567cc405ad5cf0aec943ced8cd0d9
Thiago Santana <thi.dimond1@gmail.com>
2023-10-25 11:58:08 -0300

    add theme-color metatag.

--- components/Header.js
+++ components/Header.js
@@ -6,6 +6,7 @@ function Header({ title, description }) {
       <title>{title}</title>
       <meta name="description" content={description} />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta name="theme-color" content="#000000" />
       <link rel="icon" href="/favicon.ico" />
     </Head>
   );
