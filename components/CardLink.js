4a25ff25ad4de5b5c3e839934ca119e0caee9796
Thiago Santana <thi.dimond1@gmail.com>
2023-10-25 15:36:30 -0300

    Pass false to link if no download prop provided

--- components/CardLink.js
+++ components/CardLink.js
@@ -4,14 +4,18 @@ import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 export default function CardLink({ children, title, href, download }) {
-  const fileName = href.replace(/^.*[\/]/, "");
+  const fileName = download ? href.replace(/^.*[\/]/, "") : "";
   const transform = download ? "rotate(90deg)" : "0";
   const width = download ? "min-content" : "auto";
   const alignSelf = download ? "end" : "auto";
   const marginLeft = download ? "10px" : "0";
   return (
     <div className={styles.card}>
-      <Link href={href} download={fileName} style={{ width }}>
+      <Link
+        href={href}
+        download={download ? fileName : false}
+        style={{ width }}
+      >
         <h2 className={inter.className}>
           {title}
           <div style={{ transform, alignSelf, marginLeft }}>
